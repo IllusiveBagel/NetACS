8ecce6feea501cfd2c94c912b1c526a5f29faed1
George <george@neutral.systems>
2019-08-28 21:06:57 +0100

    Added basic web server and displaying recieved and sent messages in console

---
+++ NetACS.Server/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace NetACS.Server
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
