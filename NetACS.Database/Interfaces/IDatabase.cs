c783e95ae36a97f47a2e81edc40cac31f1295dbc
George Deeley <George.Deeley@toob.co.uk>
2019-11-14 11:01:59 +0000

    #2 Added Database Project to Solution and started basic code structure

---
+++ NetACS.Database/Interfaces/IDatabase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetACS.Database.Interfaces
+{
+    public interface IDatabase
+    {
+        public T Select<T>(T model);
+        public T Insert<T>(T model);
+        public T Update<T>(T model);
+    }
+}
---
+++ NetACS.Database/Services/SQLServer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using NetACS.Database.Interfaces;
+
+namespace NetACS.Database.Services
+{
+    public class SQLServer : IDatabase
+    {
+        public T Insert<T>(T model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Select<T>(T model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Update<T>(T model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
